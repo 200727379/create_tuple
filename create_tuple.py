a79a2b89673022b88be1349d6ffe11f7f5d6449a
200727379 <rampedilesley@gmail.com>
2021-04-30 06:42:50 -0700

    initial commit

---
+++ create_tuple.py
@@ -0,0 +1,13 @@
+#Author: Mothapo Rampedi Lesley
+#Email: rampedilesley@gmail.com
+#30 April 2021
+
+"""
+Write a Python program to create a tuple.
+"""
+
+my_tuple = (1, "apple" , "mango" , "a" ,True , 666, 7 , "Python is fun") #tuple with different data type
+print(my_tuple)
+
+my_tuple1 = tuple((1, "apple" , "mango" , "a" ,True , 6, 7 , "Python is fun")) #creating the tuple using the tuple constructor
+print(my_tuple1)
